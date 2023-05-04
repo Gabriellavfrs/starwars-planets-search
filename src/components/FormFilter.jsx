e4aee2e24b2b78cb0362a1bde795a259dc91fb60
Gabriella Vieira <gabvi2015@outlook.com>
2023-05-04 17:51:05 -0300

    Requisito 7

--- src/components/FormFilter.jsx
+++ src/components/FormFilter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { AppContext } from '../context/AppProvider';
 
 function FormFilter() {
@@ -11,34 +11,37 @@ function FormFilter() {
     filterTags,
   } = useContext(AppContext);
 
-  const [filter, setfilter] = useState(
-    { column: 'population', comparison: 'maior que', number: '0' },
-  );
-
-  const initialOptions = [
+  const initialOptions = useMemo(() => [
     'population',
     'orbital_period',
     'diameter',
     'rotation_period',
-    'surface_water'];
+    'surface_water'], []);
 
   const [options, setOptions] = useState(initialOptions);
 
-  const handleFilter = ({ target }) => {
+  const [filter, setfilter] = useState(
+    { column: 'population', comparison: 'maior que', number: '0' },
+  );
+
+  const handleFilterTag = ({ target }) => {
+    // console.log(filter);
     const { name, value } = target;
     setfilter((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const handleIncludeFilter = () => {
+  const handleFilter = () => {
+    // console.log(filter);
     let filtered;
     if (filterTags.length === 0) {
+      setfilter({ ...filter, column: initialOptions[0], number: '0' });
       filtered = planetsData;
       return filtered;
     }
     filterTags.map(({ column, comparison, number }) => {
       const newOptions = options.filter((option) => option !== column);
       setOptions(newOptions);
-      // const xablau = action === 'include' ? filteredPlanets :
+      setfilter({ ...filter, column: newOptions[0], number: '0' });
       switch (comparison) {
       case ('maior que'):
         filtered = filteredPlanets.filter((tag) => Number(tag[column]) > Number(number));
@@ -58,18 +61,22 @@ function FormFilter() {
     setFilterPlanets(filtered);
   };
 
+  useEffect(() => {
+    handleFilter();
+  }, [filterTags]);
+
   const handleDeleteFilter = (tag) => {
     const newFilterTags = filterTags.filter((filterTag) => filterTag !== tag);
     setFilterTags(newFilterTags);
+    setFilterPlanets(planetsData);
+    setOptions(initialOptions);
   };
 
-  useEffect(() => {
-    handleIncludeFilter();
-  }, [filterTags]);
-
-  useEffect(() => {
-    handleIncludeFilter('delete');
-  }, [setFilterTags]);
+  const removeAllFilters = () => {
+    setFilterTags([]);
+    setFilterPlanets(planetsData);
+    setOptions(initialOptions);
+  };
 
   return (
     <div>
@@ -80,52 +87,59 @@ function FormFilter() {
           placeholder="Filtrar por nome"
           onChange={ ({ target }) => setInputText(target.value) }
         />
-        <div>
-          <label>
-            Coluna
-            <select
-              data-testid="column-filter"
-              name="column"
-              onChange={ handleFilter }
-            >
-              {options.map((option) => (
-                <option key={ option } value={ option }>{option}</option>
-              ))}
-            </select>
-          </label>
-          <label>
-            Operador
-            <select
-              data-testid="comparison-filter"
-              name="comparison"
-              onChange={ handleFilter }
-            >
-              <option value="maior que">maior que</option>
-              <option value="menor que">menor que</option>
-              <option value="igual a">igual a</option>
-            </select>
-          </label>
-          <input
-            type="number"
-            name="number"
-            value={ filter.number }
-            data-testid="value-filter"
-            onChange={ handleFilter }
-          />
-          <button
-            type="button"
-            data-testid="button-filter"
-            onClick={ () => setFilterTags([...filterTags, filter]) }
+        <label>
+          Coluna
+          <select
+            data-testid="column-filter"
+            name="column"
+            onChange={ handleFilterTag }
+            value={ options[0] }
           >
-            FILTRAR
+            {options.map((option) => (
+              <option key={ option } value={ option }>{option}</option>
+            ))}
+          </select>
+        </label>
+        <label>
+          Operador
+          <select
+            data-testid="comparison-filter"
+            name="comparison"
+            onChange={ handleFilterTag }
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <input
+          type="number"
+          name="number"
+          value={ filter.number }
+          data-testid="value-filter"
+          onChange={ handleFilterTag }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => setFilterTags([...filterTags, filter]) }
+        >
+          FILTRAR
 
-          </button>
-        </div>
+        </button>
+        <button
+          type="button"
+          data-testid="button-remove-filters"
+          onClick={ removeAllFilters }
+        >
+          REMOVER FILTROS
+
+        </button>
       </form>
       <section>
         { filterTags.length > 0 && (
           filterTags.map((tag, id) => (
-            <div key={ id }>
+            <div key={ id } data-testid="filter">
               {`${tag.column} ${tag.comparison} ${tag.number}`}
               <button type="button" onClick={ () => handleDeleteFilter(tag) }>X</button>
             </div>
